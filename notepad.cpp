73a37fe3126dcfd93c1088b85abcc188c9ca44be
ty570120 <377702575@qq.com>
2019-08-06 10:02:03 +0800

    添加菜单槽函数

--- notepad.cpp
+++ notepad.cpp
@@ -1,4 +1,4 @@
-#include "notepad.h"
+﻿#include "notepad.h"
 #include "ui_notepad.h"
 
 NotePad::NotePad(QWidget *parent) :
@@ -11,4 +11,124 @@ NotePad::NotePad(QWidget *parent) :
 NotePad::~NotePad()
 {
     delete ui;
+}
+
+/**
+ * 初始化
+ * @brief NotePad::init
+ */
+void NotePad::init(){
+    connect(ui->actionNew,SIGNAL(clicked()),this,SLOT(slotNew()));
+}
+/**
+ * @brief NotePad::loadFromFile
+ * @param fileName
+ */
+void NotePad::loadFromFile(QString fileName){
+
+}
+
+/******菜单项******/
+/*文件*/
+//新建
+void NotePad::slotNew(){
+
+}
+
+//打开
+void NotePad::slotOpen(){
+
+}
+
+//保存
+void NotePad::slotSave(){
+
+}
+//另存为
+void NotePad::slotSaveAs(){
+
+}
+//页面设置
+void NotePad::slotPageSet(){
+
+}
+//打印
+void NotePad::slotPrint(){
+
+}
+//退出
+void NotePad::slotExit(){
+
+}
+
+/*编辑*/
+//撤销
+void NotePad::slotRevoke(){
+
+}
+//剪切
+void NotePad::slotShear(){
+
+}
+//复制
+void slotCopy(){
+
+}
+//粘贴
+void NotePad::slotPaste(){
+
+}
+//删除
+void NotePad::slotDelete(){
+
+}
+//查找
+void NotePad::slotFind(){
+
+}
+//查找下一个
+void NotePad::slotFindNext(){
+
+}
+//替换
+void NotePad::slotReplace(){
+
+}
+//转到
+void NotePad::slotGoto(){
+
+}
+//全选
+void NotePad::slotSelectAll(){
+
+}
+//时间/日期
+void NotePad::slotTimeOrDate(){
+
+}
+
+/*格式*/
+//自行换行
+void NotePad::slotWrapText(){
+
+}
+//字体
+void NotePad::slotTypeface(){
+
+}
+
+/*查看*/
+//状态栏
+void NotePad::slotStatusBar(){
+
+}
+
+/*帮助*/
+//查看帮助
+void NotePad::slotViewHelp(){
+
+}
+//关于记事本
+void NotePad::slotAbout(){
+
 }
--- notepad.h
+++ notepad.h
@@ -1,4 +1,4 @@
-#ifndef NOTEPAD_H
+﻿#ifndef NOTEPAD_H
 #define NOTEPAD_H
 
 #include <QMainWindow>
@@ -14,9 +14,78 @@ class NotePad : public QMainWindow
 public:
     explicit NotePad(QWidget *parent = 0);
     ~NotePad();
+    //加载文件
+    void loadFromFile(QString fileName);
+
+public slots:
+    /******菜单项******/
+    /*文件*/
+    //新建
+    void slotNew();
+    //打开
+    void slotOpen();
+    //保存
+    void slotSave();
+    //另存为
+    void slotSaveAs();
+    //页面设置
+    void slotPageSet();
+    //打印
+    void slotPrint();
+    //退出
+    void slotExit();
+
+    /*编辑*/
+    //撤销
+    void slotRevoke();
+    //剪切
+    void slotShear();
+    //复制
+    void slotCopy();
+    //粘贴
+    void slotPaste();
+    //删除
+    void slotDelete();
+    //查找
+    void slotFind();
+    //查找下一个
+    void slotFindNext();
+    //替换
+    void slotReplace();
+    //转到
+    void slotGoto();
+    //全选
+    void slotSelectAll();
+    //时间/日期
+    void slotTimeOrDate();
+
+    /*格式*/
+    //自行换行
+    void slotWrapText();
+    //字体
+    void slotTypeface();
+
+    /*查看*/
+    //状态栏
+    void slotStatusBar();
+
+    /*帮助*/
+    //查看帮助
+    void slotViewHelp();
+    //关于记事本
+    void slotAbout();
+
+    /******文本框******/
+//    void slotCopyAvailable(bool);
+//    void slotCursorPositionChanged();
+//    void slotRedoAvailable(bool);
+//    void slotSelectionChanged();
+//    void slotTextChanged();
+//    void slotUndoAvailable(bool);
 
 private:
     Ui::NotePad *ui;
+    void init();
 };
 
 #endif // NOTEPAD_H
